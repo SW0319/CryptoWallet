f549b73b44bcd973f51dcfdf83c8b13c642b4c52
anjihun <solutionwalker0319@gmail.com>
2023-07-07 01:39:11 +0900

    기본 navigation 적용

--- app/src/main/java/com/devcat/myapplication/presentation/ui/ActivityMain.kt
+++ app/src/main/java/com/devcat/myapplication/presentation/ui/ActivityMain.kt
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentFactory
 import androidx.fragment.app.FragmentManager
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ActivityMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var a= ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
+        var a2 = supportFragmentManager.findFragmentById(R.id.mainActivity_FragmentContainerView) as NavHostFragment
+        val navController = a2.navController
+//        findViewById<BottomNavigationView>(R.id.bnv_main).setupWithNavController(a2)
     }
 }
