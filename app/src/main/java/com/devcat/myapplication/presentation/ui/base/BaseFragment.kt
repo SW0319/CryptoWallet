dcc928f721a0e119c9e94d2feedd22815da35ee5
anjihun <solutionwalker0319@gmail.com>
2023-07-12 00:41:09 +0900

    BaseFragment 및 BaseViewModel 생성

--- app/src/main/java/com/devcat/myapplication/presentation/ui/base/BaseFragment.kt
+++ app/src/main/java/com/devcat/myapplication/presentation/ui/base/BaseFragment.kt
@@ -6,13 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import com.example.myapplication.databinding.FragmentMainBinding
 
-abstract class BaseFragment<T : ViewBinding> : Fragment() {
+abstract class BaseFragment<T : ViewBinding>(private val inflate : (LayoutInflater,ViewGroup?,Boolean) -> T) : Fragment() {
 
     private var _binding : T? = null
     val binding get() = _binding!!
 
-    abstract val bindingFactory : (LayoutInflater) -> T
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +24,7 @@ abstract class BaseFragment<T : ViewBinding> : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = bindingFactory(layoutInflater)
+        _binding = inflate.invoke(inflater,container,false)
         return binding.root
     }
 
---
+++ app/src/main/java/com/devcat/myapplication/presentation/ui/base/BaseViewModel.kt
@@ -0,0 +1,9 @@
+package com.devcat.myapplication.presentation.ui.base
+
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel : ViewModel() {
+
+
+
+}
